151d3754f8b0d04ee519f785d304490e4331e6c1
Colin Francis <colinfrancis@Colins-MacBook-Pro.local>
2022-05-18 01:15:55 -0400

    Add node and print forward functionality added to Linked List class

--- main.cpp
+++ main.cpp
@@ -32,16 +32,73 @@ class Node {
         Node* getNext(void) {
             return this->next;
         }
+
+        void setNext(Node* node) {
+            this->next = node;
+        }
+
+        void setPrev(Node* node) {
+            this->prev = node;
+        }
+};
+
+class DoublyLinkedList {
+    private:
+        Node* head;
+        Node* tail;
+
+    public:
+        DoublyLinkedList(void) {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void addNode(Node* node) {
+            if (this->head == nullptr) {
+                this->head = node;
+                this->tail = this->head;
+                return;
+            }
+
+            Node* currNode = this->head;
+            while (currNode != this->tail) {
+                currNode = currNode->getNext();
+            }
+
+            currNode->setNext(node);
+            node->setPrev(currNode);
+
+            this->tail = node;
+        }
+
+        void printForward(void) {
+            Node* currNode = this->head;
+
+            cout << "LINKED LIST: ";
+
+            while (currNode != this->tail) {
+                cout << currNode->getValue() << " <-> ";
+                currNode = currNode->getNext();
+            }
+
+            cout << currNode->getValue() << endl;
+        }
 };
 
 int main(int argc, char* argv[]) {
-    Node* node = new Node(10);
+    DoublyLinkedList linkedList = DoublyLinkedList();
+    Node* node1 = new Node(10);
+    Node* node2 = new Node(20);
+    Node* node3 = new Node(30);
 
-    cout << "Node heap address: " << node << endl;
-    cout << "Node value: " << (*node).getValue() << endl;
-    cout << "Node next: " << (*node).getNext() << endl;
+    linkedList.addNode(node1);
+    linkedList.addNode(node2);
+    linkedList.addNode(node3);
+    linkedList.printForward();
 
-    delete node;
+    delete node1;
+    delete node2;
+    delete node3;
 
     return 0;
 }
